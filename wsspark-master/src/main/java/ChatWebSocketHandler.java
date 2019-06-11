90014157f55cfa3343b16ee2a33e722d9dc73c42
KushKong <kushariadikong@gmail.com>
2019-06-11 20:29:17 +0700

    Add files via upload
    UTS FAST TRACK SISTEM TERDISTRIBUSI

---
+++ wsspark-master/src/main/java/ChatWebSocketHandler.java
@@ -0,0 +1,59 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import static com.mashape.unirest.http.Unirest.get;
+import static java.nio.file.Paths.get;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import static spark.Spark.get;
+
+@WebSocket
+public class ChatWebSocketHandler {
+    private String sender, msg;
+
+    @OnWebSocketConnect
+    public void onConnect(Session user) throws Exception {
+        String username = "User" + Chat.nextUserNumber++;
+        Chat.userUserNameMap.put(user, username);
+        Chat.broadcastMessage(user, "Server", (username + " joined the chat"));
+    }
+
+    @OnWebSocketClose
+    public void onClose(Session user, int statusCode, String reason) {
+        String username = Chat.userUserNameMap.get(user);
+        Chat.userUserNameMap.remove(user);
+        Chat.broadcastMessage(user, "Server", (username + " left the chat"));
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(Session user, String message) {
+        //String username = Chat.userUserNameMap.get(user);
+        //Chat.broadcastMessage(username, message);
+        JSONObject json = new JSONObject(message);
+        String nama = (String)json.get("nama");
+        String pesan = (String)json.get("pesan");
+
+        try {
+            HttpResponse<JsonNode> response = Unirest.get("https://ghibliapi.herokuapp.com/locations").asJson();
+            JSONArray array = response.getBody().getArray();
+            
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < array.length(); i++) {
+                
+            
+                String name = array.getJSONObject(i).getString("name");
+                if(name.toLowerCase().contains(pesan.toLowerCase())){
+      
+            Chat.broadcastMessage(user, nama, name);
+        }}
+        }catch (Exception e) {
+            Chat.broadcastMessage(user, nama, "Request tidak dapat dilakukan");
+        }
+
+    }
+}
